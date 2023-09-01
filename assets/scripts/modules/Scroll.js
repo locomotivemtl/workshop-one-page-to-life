fd28fb6bbed226880cf2749d7cd503a7bb83d94a
Deven Caron <devencaron@gmail.com>
2023-09-01 10:27:37 -0400

    Improve scroll.js for readability

--- assets/scripts/modules/Scroll.js
+++ assets/scripts/modules/Scroll.js
@@ -9,16 +9,10 @@ export default class extends module {
     }
 
     init() {
-        this.scroll = new LocomotiveScroll({
+        this.locomotiveScroll = new LocomotiveScroll({
             modularInstance: this,
             scrollCallback: this.onScrollBind,
         })
-
-        // // Force scroll to top
-        // if (history.scrollRestoration) {
-        //     history.scrollRestoration = 'manual'
-        //     window.scrollTo(0, 0)
-        // }
     }
 
     onScroll({ scroll, limit, velocity, direction, progress }) {
@@ -26,19 +20,19 @@ export default class extends module {
         window.locomotiveScrollData = { scroll, limit, velocity, direction, progress };
     }
 
+    /**
+     * Scroll to
+     *
+     * @param {object}
+     */
     scrollTo(params) {
-        let { target, ...options } = params
+        let { target, options } = params
 
-        options = Object.assign({
-            // Defaults
-            duration: 1,
-        }, options)
-
-        this.scroll?.scrollTo(target, options)
+        this.locomotiveScroll?.scrollTo(target, options)
     }
 
     destroy() {
-        super.destroy()
-        this.scroll?.destroy();
+        super.destroy();
+        this.locomotiveScroll?.destroy();
     }
 }
