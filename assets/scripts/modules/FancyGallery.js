161902e0c50cfd3e2858b250d72213cc533885e2
Jérémy Minié <jeremy.minie@gmail.com>
2023-09-13 11:26:13 -0400

    Remove files & code for chapter-4/step-timeline-setup

--- assets/scripts/modules/FancyGallery.js
+++ assets/scripts/modules/FancyGallery.js
@@ -1,42 +1,16 @@
-import gsap from 'gsap'
 import { module } from 'modujs'
-import { CUSTOM_EVENT } from '../config';
 
 export default class extends module {
     constructor(m) {
         super(m);
-
-        // Element Selectors
-        this.$sticky = this.$('sticky')[0];
-        this.$grid = this.$('grid')[0];
-        this.$gridItems = Array.from(this.$('grid-item'));
-        this.$images = Array.from(this.$('image'));
     }
 
     init() {
-        // Start timeline stuff
-        this.computeTl();
+        // Nothing yet
     }
 
-    computeTl() {
-        // Create the timeline
-        this.tl = gsap.timeline({});
-
-        // Rotate our first grid item
-        this.tl.to(this.$gridItems[0], { rotateZ: '360deg' })
-
-        this.tl.progress(0); // Set progress to 0
-        this.tl.pause(); // Very important to pause the timeline, we don't want it to play on its own: it's meant to be controlled by `onScrollProgress` only
-    }
-
-    // Allows to progress through our timeline relatively to the data-scroll progress given
+    // This method will be called automatically by `data-scroll-module-progress`
     onScrollProgress(value) {
-        this.tl?.progress?.(value);
-    }
-
-    destroy() {
-        super.destroy();
-
-        this.tl?.kill?.();
+        console.log(value)
     }
 }
