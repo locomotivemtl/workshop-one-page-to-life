4c34b260fdf89dfd4405bef8f3c54223f22d719b
Jérémy Minié <jeremy.minie@gmail.com>
2023-09-16 13:50:01 -0400

    Remove some code for `chapter-6/step-timeline`

--- assets/scripts/modules/SplashCanvas.js
+++ assets/scripts/modules/SplashCanvas.js
@@ -1,11 +1,10 @@
-import { module as Module } from 'modujs'
-import gsap from 'gsap'
+import { module as module } from 'modujs'
 import * as THREE from 'three';
 import { gltfLoader, textureLoader } from '../utils/3d'
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js'
 import { CUSTOM_EVENT } from '../config';
 
-export default class extends Module {
+export default class extends module {
     constructor(m) {
         super(m)
 
@@ -22,7 +21,6 @@ export default class extends Module {
     init() {
         this.initRenderer();
         this.initScene();
-        this.initTimeline();
         this.loadModel();
 
         // Call a second time after init to impact THREE js objects
@@ -54,14 +52,6 @@ export default class extends Module {
         this.wrapper.position.set(0,0,0);
         this.scene.add(this.wrapper)
 
-        // const pointLight = new THREE.PointLight(0x0000ff, 4000)
-        // pointLight.position.set( 20, 20, 20 );
-        // pointLight.castShadow = true; // default false
-        // this.scene.add(pointLight)
-
-        // const axesHelper = new THREE.AxesHelper( 1 );
-        // this.scene.add( axesHelper );
-
         // ==========================================================================
         // CAMERA
         // ==========================================================================
@@ -75,34 +65,6 @@ export default class extends Module {
         this.controls = new OrbitControls(this.camera, this.canvas)
         this.controls.enableDamping = true
         this.controls.enableZoom = false
-    }
-
-    initTimeline() {
-        if(!this.camera) return
-
-        // Create the timeline
-        this.tl = gsap.timeline({})
-
-        // Move the camera from origin to target
-        this.tl.fromTo(
-            this.camera.position,
-            {
-                x: 15,
-                y: 25,
-                z: 15
-            }, {
-                x: 15,
-                y: -5,
-                z: 15,
-                ease: 'linear'
-            }
-        );
-        // Because we're using OrbitControls, the camera will keep targeting the center of the scene.
-        // We could get rid of the OrbitControls and manually call camera.lookAt() in the render loop instead
-
-        // Usual tl calls
-        this.tl.progress(0);
-        this.tl.pause()
     }
 
     loadModel() {
@@ -175,13 +137,8 @@ export default class extends Module {
         this.raf = window.requestAnimationFrame(this.render.bind(this))
     }
 
-    onScrollProgress(progress) {
-        this.tl?.progress?.(progress)
-    }
-
     destroy() {
         super.destroy();
-        this.tl?.kill?.();
         window.removeEventListener(CUSTOM_EVENT.RESIZE_END, this.resize);
         window.cancelAnimationFrame(this.raf)
         this.scene = this.renderer = null
