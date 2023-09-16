69eddd1157e464eacdb634877dca853edacb79a4
Jérémy Minié <jeremy.minie@gmail.com>
2023-09-16 14:00:38 -0400

    Add & import new files to `chapter-6/start`

---
+++ assets/scripts/modules/SplashCanvas.js
@@ -0,0 +1,15 @@
+import { module } from 'modujs';
+
+export default class extends module {
+    constructor(m) {
+        super(m);
+    }
+
+    init() {
+        // do your magic here
+    }
+
+    destroy() {
+        super.destroy()
+    }
+}
